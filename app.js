e4f17164b9923d3d1edeef36749b68ce261330c4
adojem <adojemcovix@gmail.com>
2018-07-31 17:25:43 +0900

    Display the Profile

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Init GitHub
 const github = new GitHub();
+// Init UI
+const ui = new UI();
 
 // Search input
 const searchUser = document.getElementById('searchUser');
@@ -19,6 +21,7 @@ searchUser.addEventListener('keyup', (e) => {
             }
             else {
                // Show profile
+               ui.showProfile(data.profile)
             }
          });
    }
